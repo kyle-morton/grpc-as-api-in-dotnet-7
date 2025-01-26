4532299db8713e6f15847152fc7934f969c2664d
Kyle Morton <mkmorton91@gmail.com>
2025-01-26 16:55:46 -0600

    Adding read endpoint logic

--- ToDoGrpc/Services/ToDoService.cs
+++ ToDoGrpc/Services/ToDoService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Grpc.Core;
+using Microsoft.EntityFrameworkCore;
 using ToDoGrpc.Data;
 using ToDoGrpc.Models;
 
@@ -16,6 +17,11 @@ public class ToDoService : ToDoIt.ToDoItBase
         _dbContext = dbContext;
     }
 
+    /*
+    * This class is an implementation of the grpc contract we create in the proto file (see protos folder)
+    * Each method here is an override of the methods there w/ actual logic to connect it up to a DB.
+    */
+    
     public override async Task<CreateToDoResponse> CreateToDo (CreateToDoRequest request, ServerCallContext context) 
     {
         if (request.Title == string.Empty || request.Description == string.Empty) {
@@ -33,6 +39,26 @@ public class ToDoService : ToDoIt.ToDoItBase
 
         return await Task.FromResult(new CreateToDoResponse {
             Id = todoItem.Id
+        });
+    }
+
+    public override async Task<ReadToDoResponse> ReadToDo(ReadToDoRequest request, ServerCallContext context)
+    {
+        if (request.Id <= 0) {
+            throw new RpcException(new Status(StatusCode.InvalidArgument, "Resource Id must be greater than 0"));            
+        }
+
+        var todoItem = await _dbContext.ToDoItems.FirstOrDefaultAsync(t => t.Id == request.Id);
+
+        if (todoItem == null) {
+            throw new RpcException(new Status(StatusCode.NotFound, "No resource found with given ID"));
+        }
+
+        return await Task.FromResult(new ReadToDoResponse{
+            Id = todoItem.Id,
+            Description = todoItem.Description,
+            Title = todoItem.Title,
+            ToDoStatus = todoItem.ToDoStatus.ToString()
         });
     }
 
